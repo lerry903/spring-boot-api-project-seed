2bf0bcd65a8d9444b2f20b24c4633460772a9bcd
lerry903 <lerry@foxmail.com>
2018-06-19 16:33:00 +0800

    SpringBoot去除内嵌tomcat

--- src/main/java/com/company/project/Application.java
+++ src/main/java/com/company/project/Application.java
@@ -3,15 +3,26 @@ package com.company.project;
 import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * @author lerry
  */
 @EnableSwagger2Doc
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    /**
+     * 实现SpringBootServletInitializer可以让spring-boot项目在web容器中运行
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        builder.sources(this.getClass());
+        return super.configure(builder);
     }
 }
 
